53b51cd55cbdd66184f3a4e6af4e75df0ef7031b
Luke Olson <lolson@finetune.com>
2026-07-24 17:31:45 -0700

    Add per-user api_token via has_secure_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_secure_token :api_token
   has_many :sessions, dependent: :destroy
   has_many :collection_items, dependent: :destroy
   has_many :vibe_overrides, dependent: :destroy
@@ -10,4 +11,6 @@ class User < ApplicationRecord
   encrypts :discogs_token
 
   validates :email_address, presence: true, uniqueness: true
+
+  alias_method :regenerate_api_token!, :regenerate_api_token
 end
